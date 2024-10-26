ba933844e8c4eb611683eccb2b37e58fc66bf01a
Casmir Onyeka <casmir293@gmail.com>
2024-10-26 23:40:53 +0100

    array bubble sort

--- dsa.js
+++ dsa.js
@@ -1,19 +1,40 @@
 let myArray = [7, 12, 9, 11, 3];
 
-/** @FIND THE MINIMUM VALUE IN THE ARRAY
- * save the first data
- * loop through and compare the next item with the saved item
- * if the data is lower than the saved, update the saved data
+/** @ARRAY MINIMUM VALUE
+ * save the first value
+ * loop through and compare the next value with the saved value
+ * if the value is lower than the saved, update the saved value
  */
+function minimumValue(arr) {
+  let lowestValue = arr[0];
 
-let lowestData = myArray[0];
-
-for (let i = 1; i <= myArray.length; i++) {
-  if (myArray[i] < lowestData) {
-    lowestData = myArray[i];
+  for (let i = 1; i <= arr.length; i++) {
+    if (arr[i] < lowestValue) {
+      lowestValue = arr[i];
+    }
   }
+  console.log("Lowest Value ==>", lowestValue);
 }
 
-console.log("Lowest Value:", lowestData);
+/** @ARRAY BUBBLE SORT
+ * loop through the array
+ * check if initial value is greater than the next value
+ * if true, swap their index positions and assign new initial value
+ */
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let swapped = false;
 
-// BUBBLE SORT
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+
+        swapped = true;
+      }
+    }
+    if (!swapped) break;
+  }
+  return console.log("Bubble Sort ==>", arr);
+}
