3ac8d31371d27956a0ec3d571eb6fb85d0be2fa4
Casmir Onyeka <casmir293@gmail.com>
2024-10-27 12:54:10 +0100

    array selection sort

--- array.js
+++ array.js
@@ -5,7 +5,7 @@ let myArray = [7, 12, 9, 11, 3];
  * loop through and compare the next value with the saved value
  * if the value is lower than the saved, update the saved value
  */
-function minimumValue(arr) {
+const minimumValue = (arr) => {
   let lowestValue = arr[0];
 
   for (let i = 1; i <= arr.length; i++) {
@@ -14,16 +14,17 @@ function minimumValue(arr) {
     }
   }
   console.log("Lowest Value ==>", lowestValue);
-}
+  return arr;
+};
 
 /** @ARRAY BUBBLE SORT
  * loop through the array
  * check if initial value is greater than the next value
  * if true, swap their index positions and assign new initial value
  */
-function bubbleSort(arr) {
+const bubbleSort = (arr) => {
   for (let i = 0; i < arr.length - 1; i++) {
-    let swapped = false;
+    let isSwapped = false;
 
     for (let j = 0; j < arr.length - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
@@ -31,10 +32,36 @@ function bubbleSort(arr) {
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
 
-        swapped = true;
+        isSwapped = true;
       }
     }
-    if (!swapped) break;
+    if (!isSwapped) break;
   }
-  return console.log("Bubble Sort ==>", arr);
-}
+  console.log("Bubble Sort ==>", arr);
+  return arr;
+};
+
+/** @ARRAY SELECTION SORT
+ * loop through the array
+ * find the lowest value
+ * move it to the start of the loop by swapping its index
+ */
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      let temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+  console.log("Selection Sort ==>", arr);
+  return arr;
+};
